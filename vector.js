6e6acd380a11c1e15394eb47cdc9c873c4389473
suicidepills <atom.mac@gmail.com>
2012-06-23 16:02:30 -0400

    Initial commit.  This is a port of my original Pitri code to the Crafty game engine.  This is primarily so this project can take advantage of Crafty's collision detection (and the Box2D module) and some other helpful utilities that the framework offers.

---
+++ vector.js
@@ -0,0 +1,82 @@
+/*
+ * Vector Class
+ * 
+ * Borrowed from a kind soul at pastbin.com
+ * http://pastebin.com/h5PSDR0g
+ *
+ * Thank you!
+ */
+Vector = function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+   
+	// Calculate the length of a the vector
+	// @param void
+	// @return vector
+	this.len = function Length(newLen) {
+		if(!isNaN(newLen)) {
+			var length = this.len();
+			var xnorm = this.x / length;
+			var ynorm = this.y / length;
+			this.x = xnorm * newLen;
+			this.y = ynorm * newLen;
+			
+		}
+		return Math.sqrt((this.x * this.x) + (this.y * this.y));
+	}
+	
+	// Truncate vector.
+	this.trunc = function Truncate(maxLen) {
+		if (this.len() > maxLen) {
+			this.len(maxLen);
+		}
+	}
+	
+	// Add 2 vectors
+	this.add = function Add(v) {
+		return new Vector(this.x + v.x, this.y + v.y);
+	}
+   
+	// Substract 2 vectors
+	// @param v A vector
+	// @return vector
+	this.sub = function Substract(v) {
+			return new Vector(this.x - v.x, this.y - v.y);
+	}
+   
+	// Calculate a vector dot product
+	// @param v A vector
+	// @return The dot product
+	this.dot = function DotProduct(v) {
+			return (this.x * v.x + this.y * v.y);
+	}
+	
+	// Multiply 2 vectors
+	this.mult = function Multiply(n) {
+		return new Vector(this.x*n, this.y*n)
+	}
+   
+	// Normalize the vector
+	// http://www.fundza.com/vectors/normalize/index.html
+	// http://programmedlessons.org/VectorLessons/vch04/vch04_4.html
+	// @param void
+	// @return vector
+	this.normalize = function Normalize() {
+			var length = this.len();
+			this.x = this.x / length;
+			this.y = this.y / length;
+	}
+   
+	// Calculate the perpendicular vector (normal)
+	// http://en.wikipedia.org/wiki/Perpendicular_vector
+	// @param void
+	// @return vector
+	this.perp = function Perp() {
+			return new Vector(-this.y, this.x);
+	}
+	
+	// Create and return a copy of this Vector.
+	this.copy = function Copy() {
+		return new Vector(this.x, this.y);
+	}
+}
